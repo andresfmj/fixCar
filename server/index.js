f3df7d61188981b98434dce21f2f35394ed8a1bd
Andres Fernando <andres.fernando08@gmail.com>
2020-09-14 21:54:52 -0500

    configuracion archivo inicial

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express   = require('express');
+const morgan    = require('morgan');
+const cors      = require('cors');
+
+const env       = require('dotenv');
+env.config();
+
+const app       = express();
+const routes    = require('./routes/index');
+
+require('./config/database');
+
+app.use(morgan(process.env.NODE_ENV === 'env' ? 'dev' : 'combined'))
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/', routes);
+
+app.set('port', process.env.APP_PORT || 3131);
+
+app.listen(app.get('port'), () => {
+    console.log(`Server API is running at port ${app.get('port')}`);
+});
