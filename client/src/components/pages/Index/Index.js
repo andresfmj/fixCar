8f2cd29fc13ea8f65b19233fc4561656aad2c65d
Andres Fernando <andres.fernando08@gmail.com>
2020-09-15 22:41:02 -0500

    refactorizado el listado de clientes

--- client/src/components/pages/Index/Index.js
+++ client/src/components/pages/Index/Index.js
@@ -1,25 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
-import { fetchClientsList } from '../../../store/actions';
+import Client from '../../UI/Clients/Clients';
+import ClientRow from '../../UI/Clients/ClientRow';
 
 function Index(props) {
-    const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-        props.fetchClientsList()
-    }, [])
 
     return (
         <div className='Index'>
-            <h2>Index Page</h2>
-            <p>WIP</p>
             {props.loading ? <p>Loading clients...</p> : null}
-            <ul>
-                {props.clients && props.clients.map(i => (
-                    <li key={i._id}>{`${i.names} ${i.lastNames}`}</li>
-                ))}
-            </ul>
+            <Client>
+                <ClientRow clients={props.clients} />
+            </Client>
         </div>
     )
 }
@@ -32,8 +24,4 @@ const mapStateToProps = state => (
     }
 )
 
-const mapDispathToProps = {
-    fetchClientsList
-}
-
-export default connect(mapStateToProps, mapDispathToProps)(Index);
+export default connect(mapStateToProps)(Index);
