18fa7ad56807e7d8f8b03f4274b56d3035b759bc
Andres Fernando <andres.fernando08@gmail.com>
2020-09-15 21:35:01 -0500

    creado componente de Toolbar

---
+++ client/src/components/UI/Toolbar/Toolbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './Toolbar.scss';
+
+function Toolbar(props) {
+
+    return (
+        <nav className='Toolbar'>
+            <div className="nav">
+                <header>
+                    <h2>
+                        <a href="#">Control de Autos</a>
+                    </h2>
+                </header>
+            </div>
+        </nav>
+    )
+}
+
+export default Toolbar;
