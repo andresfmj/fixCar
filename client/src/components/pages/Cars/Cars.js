ec35fea45b5b32d94adaa5c8b47cfa8d7297125d
Andres Fernando <andres.fernando08@gmail.com>
2020-09-16 11:59:25 -0500

    enlazado modal y dentro formulario para registrar auto

--- client/src/components/pages/Cars/Cars.js
+++ client/src/components/pages/Cars/Cars.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { createPortal } from 'react-dom';
 import { connect } from 'react-redux';
-import { withRouter, useParams } from 'react-router-dom';
+import { withRouter, useParams, useRouteMatch } from 'react-router-dom';
+import NewCar from '../../NewCar/NewCar';
+import Modal from '../../UI/Modal/Modal';
+import Overlay from '../../UI/Overlay/Overlay';
 
 import './Cars.scss';
 
 function Cars(props) {
+    const [newCarModal, setNewCarModal] = useState(false)
+
     const routeParams = useParams()
 
     let selectedClient = props.clients.length > 0 ? props.clients.find(v => v._id === routeParams.clientId) : null
     let carsByClient = selectedClient && selectedClient.car && selectedClient.car.map(c => {
         return c
     })
+
+    const toggleNewCarWindow = () => {
+        setNewCarModal(!newCarModal)
+    }
 
     return (
         <div className="Cars"> 
@@ -21,7 +31,11 @@ function Cars(props) {
                             <React.Fragment>
                                 <header>
                                     <h2>Autos de {`${selectedClient.names} ${selectedClient.lastNames}`}</h2>
-                                    <button className="button button-primary">nuevo auto</button>
+                                    <button className="button button-primary" onClick={toggleNewCarWindow}>nuevo auto</button>
+                                    { createPortal(<Overlay show={newCarModal} clicked={toggleNewCarWindow} />, document.getElementById('modal')) }
+                                    <Modal show={newCarModal}>
+                                        <NewCar client={selectedClient} />
+                                    </Modal>
                                 </header>
                                 <div className='CarsList'>
                                     <ul>
